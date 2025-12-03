894457edf0e5df6a2f846f1f251c31b4fd33feb2
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-12-02 23:44:34 -0300

    feat: implement other screens

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
